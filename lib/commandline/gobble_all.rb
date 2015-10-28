3f7bd2c2ee1922273ea242aec44402f5f355d263
Andre Dantas Rocha <andre.dantas.rocha@envato.com>
2015-10-28 12:01:53 +1100

    Making gobble run form IDE, processing only specific folders, separate yml files

--- lib/commandline/gobble_all.rb
+++ lib/commandline/gobble_all.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 class GobbleAll
   include GobbleShare
   
@@ -32,7 +34,7 @@ class GobbleAll
     text_extractor = Tr8nTextExtractor.new
     
     Dir["#{rails_view_directory}/**/*#{@extension}" ].each do |full_erb_file_path|
-      
+
       erb_file = full_erb_file_path.gsub( @rails_root, '' )
       erb_file = ErbFile.load( full_erb_file_path )
       erb_file.extract_text( text_extractor )
@@ -44,15 +46,27 @@ class GobbleAll
   end
   
   def execute_i18n
-    locale_file_name = "en.yml"
-    
     rails_view_directory = "#{@rails_root}/app/views"
-    full_yml_file_path = "#{@rails_root}/config/locales/#{locale_file_name}"
-    
-    rails_translation_store = RailsTranslationStore.load_from_file( full_yml_file_path )
-    text_extractor = RailsTextExtractor.new( rails_translation_store )
-    
+
     Dir["#{rails_view_directory}/**/*#{@extension}" ].each do |full_erb_file_path|
+
+      if !(full_erb_file_path.start_with? '/Users/andredantasrocha/src/marketplace/app/views/item') &&
+        !(full_erb_file_path.start_with? '/Users/andredantasrocha/src/marketplace/app/views/buying')
+        puts "Skipping #{full_erb_file_path}"
+        next
+      end
+
+      # if /gone\.html\.erb$/ !~full_erb_file_path
+      #   next
+      # end
+
+      yml_relative_path = full_erb_file_path.gsub("#{@rails_root}/app/", '')
+      en_yml_relative_path = yml_relative_path.gsub('html.erb', 'en.yml')
+
+      full_yml_file_path = "#{@rails_root}/config/locales/#{en_yml_relative_path}"
+      rails_translation_store = RailsTranslationStore.load_from_file( full_yml_file_path )
+      text_extractor = RailsTextExtractor.new( rails_translation_store )
+
       erb_file = full_erb_file_path[@rails_root.length,full_erb_file_path.length]
       rails_translation_store.start_new_context( convert_path_to_key_path( erb_file.to_s ) )
       erb_file = ErbFile.load( full_erb_file_path )
@@ -60,10 +74,13 @@ class GobbleAll
 
       File.open(full_erb_file_path, 'w') {|f| f.write(erb_file.to_s) }
       puts "Wrote #{full_erb_file_path}"
-    end
 
-    File.open(full_yml_file_path, 'w') {|f| f.write(rails_translation_store.serialize) }
-    puts "Wrote #{full_yml_file_path}"    
+      yml_file_dir  = File.dirname(full_yml_file_path)
+      FileUtils.mkdir_p(yml_file_dir) unless File.exists?(yml_file_dir)
+
+      File.open(full_yml_file_path, 'w') {|f| f.write(rails_translation_store.serialize) }
+      puts "Wrote #{full_yml_file_path}"
+    end
   end
   
 end
